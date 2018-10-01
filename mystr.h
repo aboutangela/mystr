ade38b1e5dac2d4c5e7b79ffa66916becd46e682
George <gliang90@stuy.edu>
2018-09-30 20:11:59 -0400

    added my headers

--- mystr.h
+++ mystr.h
@@ -1,11 +1,13 @@
 int mylen(char *s);
 
 
-
 char * mycat(char *dest, char *source);
-
 
 
 int mycmp(char *s1, char *s2);
 
 
+char * mystrncpy(char *dest, char *source, int n);
+
+
+char * mystrchr(char *s, char c);
